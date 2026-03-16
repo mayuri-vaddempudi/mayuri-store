4201b26e8da50c4c0d15ebe9c7773143e36d73bd
Mayuri Yedlapalli <mayuri@example.com>
2026-03-16 15:21:11 +0100

    latest changes

--- src/components/Womens/index.jsx
+++ src/components/Womens/index.jsx
@@ -1,10 +1,15 @@
 import styles from "./womens.module.css";
+import "@fontsource/dancing-script";
 
 const Womens = () => {
   return (
     <div className={styles.womens}>
-      <h3>Women's fashion</h3>
-      <li>
+      <h3 className={styles.dancingScript}>Women's fashion</h3>
+      <p className={styles.womenDiscription}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry.
+      </p>
+      <li className={styles.womenShoping}>
         <a>ShopNow</a>
       </li>
     </div>
